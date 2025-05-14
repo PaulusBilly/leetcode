8a5f0a8ec8799b1905f3eae3d1876f39ff491285
pBilly <141384133+PaulusBilly@users.noreply.github.com>
2025-05-14 11:33:24 +0700

    Time: 1 ms (81.21%), Space: 14.7 MB (50.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> umap;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
+        unordered_map<int, int> hashmap;
+
+        for (int i = 0; i < nums.size(); i++) {
             int x = target - nums[i];
-            if (umap.contains(x)) {
-                return{umap[x], i};
+            if (hashmap.count(x)) {
+                return {hashmap[x], i};
             }
-            umap[nums[i]] = i;
+            hashmap[nums[i]] = i;
         }
         return {};
     }
